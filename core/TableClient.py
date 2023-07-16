3e0bc265afe59f132d1d05e69f078f8142daf1d3
Serzho <Kudrsv.kudrsv@gmail.com>
2023-07-16 14:09:54 +0300

    changed connection to google spreadsheets to gspread

--- core/TableClient.py
+++ core/TableClient.py
@@ -1,46 +1,51 @@
 from abc import ABC, abstractmethod
-import httplib2
-import apiclient.discovery
-from oauth2client.service_account import ServiceAccountCredentials
-from cfg import EMAIL
+import gspread
+from gspread import Client, Spreadsheet
 
-TABLE_TOKEN_FILE = "table_token.json"
+from cfg import EMAIL, TABLE_TOKEN_FILE
+
 
 class TableClient(ABC):
+    __slots__ = ["_sheet"]
+
     @abstractmethod
     def __init__(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def create_table(self):
+    def _connect_table(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def write_flb(self, flb_list: list[tuple]):
         pass
 
 
 class GoogleSheetsClient(TableClient):
     def __init__(self):
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(TABLE_TOKEN_FILE,
-                                                                       ['https://www.googleapis.com/auth/spreadsheets',
-                                                                        'https://www.googleapis.com/auth/drive'])
-        http_auth = credentials.authorize(httplib2.Http())
-        service = apiclient.discovery.build('sheets', 'v4', http=http_auth)
-        print(service.spreadsheets())
-        spreadsheet = service.spreadsheets().create(body={
-            'properties': {'title': 'Первый тестовый документ', 'locale': 'ru_RU'},
-            'sheets': [{'properties': {'sheetType': 'GRID',
-                                       'sheetId': 0,
-                                       'title': 'Лист номер один',
-                                       'gridProperties': {'rowCount': 100, 'columnCount': 15}}}]
-        }).execute()
-        spreadsheet_id = spreadsheet['spreadsheetId']  # сохраняем идентификатор файла
-        drive_service = apiclient.discovery.build('drive', 'v3', http=http_auth)
+        gs_client = gspread.service_account(filename=TABLE_TOKEN_FILE)
+        self._sheet: Spreadsheet
+        self._connect_table(gs_client, "Bonds")
 
-        access = drive_service.permissions().create(
-            fileId=spreadsheet_id,
-            body={'type': 'user', 'role': 'writer', 'emailAddress': EMAIL},
-            # Открываем доступ на редактирование
-            fields='id'
-        ).execute()
-        print('https://docs.google.com/spreadsheets/d/' + spreadsheet_id)
+    def _connect_table(self, gs_client: Client, table_title: str):
+        if table_title not in [sheet.title for sheet in gs_client.openall()]:
+            gs_client.create(table_title)
+            self._sheet = gs_client.open(table_title)
+            self._sheet.add_worksheet(title="Main", rows=100, cols=20)
+        else:
+            self._sheet = gs_client.open(table_title)
+        self._sheet.share(EMAIL, perm_type='user', role='writer')
 
-    def create_table(self):
-        pass
+    def write_flb(self, flb_list: list[tuple]):
+        worksheet = self._sheet.worksheet("Main")
+        worksheet.update("A1:J1", ['Тикер', 'Название', 'Валюта', 'Дата размещения',
+                                   'Дата погашения', 'Времени до погашения', 'Количество купонов в год',
+                                   'Цена', 'Номинал', 'Реальная доходность'])
+        for row_ind, fld_tuple in enumerate(flb_list):
+            for col_ind in range(1, 11):
+                worksheet.update(row_ind + 2, col_ind, fld_tuple[col_ind])
+
+
+
+
+
