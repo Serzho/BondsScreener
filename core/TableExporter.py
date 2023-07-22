c90f7383fe739d11013bb5b43503b6970658e5c4
Serzho <Kudrsv.kudrsv@gmail.com>
2023-07-23 01:50:33 +0300

    added effective profitability

--- core/TableExporter.py
+++ core/TableExporter.py
@@ -33,35 +33,59 @@ class TableExporter:
         return out
 
     @staticmethod
-    def _count_annual_profitability(date_dt: (int, int), coupons: list[dict], nominal_price: float, real_price: float,
-                                    aci: float, currency: str) -> float:
-        if currency != "rub":
-            return 0.
-
-        years, months = date_dt
-        expenses = real_price + real_price * COMMISSION + aci
+    def _count_proceeds_and_expenses(today: datetime.date, coupons: list[dict], nominal_price: float, real_price: float,
+                                     aci: float, scale: float = 1.) -> (float, float):
+        expenses = (real_price + real_price * COMMISSION + aci) * scale
         total_coupons = 0
-        today = datetime.date.today()
 
         for coupon in coupons:
-            if today > coupon.get("date"):
+            if today >= coupon.get("date"):
                 continue
-            total_coupons += coupon.get("value") * 0.87
+            total_coupons += coupon.get("value") * 0.87 * scale
 
         repayment = nominal_price
         if nominal_price > real_price:
-            repayment -= (nominal_price - real_price) * 0.13
+            repayment -= (nominal_price - real_price) * 0.13 * scale
 
         proceeds = repayment + total_coupons
-        if expenses == 0:
-            return 0.
+        return proceeds, expenses
+
+    def _count_simple_profitability(self, date_dt: (int, int), coupons: list[dict], nominal_price: float,
+                                    real_price: float, aci: float, currency: str) -> float:
+        years, months = date_dt
+        today = datetime.date.today()
+        proceeds, expenses = self._count_proceeds_and_expenses(today, coupons, nominal_price, real_price, aci)
+
+        total_profit = proceeds / expenses
+        return round(100 * total_profit ** (12 / (years * 12 + months)) - 100, 2)
+
+    def _count_effective_profitability(self, date_dt: (int, int), coupons: list[dict], nominal_price: float,
+                                       real_price: float,
+                                       aci: float, currency: str) -> float:
+        years, months = date_dt
+        today = datetime.date.today()
+        proceeds, expenses = self._count_proceeds_and_expenses(today, coupons, nominal_price, real_price, aci)
+        today = datetime.date.today()
+
+        for coupon in coupons:
+            if today >= coupon.get("date"):
+                continue
+            coupon_proceeds, coupon_expenses = self._count_proceeds_and_expenses(
+                coupon.get("date"), coupons, nominal_price, real_price, aci, 0.87 * coupon.get("value") / real_price
+            )
+            proceeds += coupon_proceeds
+            expenses += coupon_expenses
 
         total_profit = proceeds / expenses
         return round(100 * total_profit ** (12 / (years * 12 + months)) - 100, 2)
 
     def _get_row_list(self, bond_dict: dict) -> list:
         date_diff = self._date_dt(datetime.date.today(), bond_dict.get("maturity_date"))
-        annual_profit = self._count_annual_profitability(
+        simple_profit = self._count_simple_profitability(
+            date_diff, bond_dict.get("coupons"), bond_dict.get("nominal_value"), bond_dict.get("real_value"),
+            bond_dict.get("aci"), bond_dict.get("currency")
+        )
+        effective_profit = self._count_effective_profitability(
             date_diff, bond_dict.get("coupons"), bond_dict.get("nominal_value"), bond_dict.get("real_value"),
             bond_dict.get("aci"), bond_dict.get("currency")
         )
@@ -70,7 +94,7 @@ class TableExporter:
             bond_dict.get("ticker"), bond_dict.get("name"), bond_dict.get("currency"), bond_dict.get("risk_level"),
             bond_dict.get("placement_date").strftime("%d-%m-%Y"), bond_dict.get("maturity_date").strftime("%d-%m-%Y"),
             self._prepare_date_dt(*date_diff), bond_dict.get("coupon_quantity_per_year"), bond_dict.get("real_value"),
-            bond_dict.get("nominal_value"), annual_profit
+            bond_dict.get("nominal_value"), simple_profit, effective_profit
         ]
 
     def get_table(self, bond_list: list[dict]) -> list[list]:
